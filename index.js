40f7b7932b0ba289685a8237cd15d6df40f4e687
Jasdeep Singh <j.singhhh917@gmail.com>
2025-03-22 17:32:24 +1100

    refactoring via new unit variables

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const { Engine, Render, Runner, World, Bodies, Body, Events } = Matter;
 
-const cells = 3;
+const cellsHorizontal = 10;
+const cellsVertical = 9;
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const unitLength = width / cells;
+const unitLengthX = width / cellsHorizontal;
+const unitLengthY = height / cellsVertical;
 
 const engine = Engine.create();
-engine.world.gravity.y = 0
+engine.world.gravity.y = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -32,7 +34,7 @@ World.add(world, walls);
 
 // Maze generation
 
-const shuffle = (arr) => {
+const shuffle = arr => {
   let counter = arr.length;
 
   while (counter > 0) {
@@ -48,20 +50,20 @@ const shuffle = (arr) => {
   return arr;
 };
 
-const grid = Array(cells)
+const grid = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells).fill(false));
+  .map(() => Array(cellsHorizontal).fill(false));
 
-const verticals = Array(cells)
+const verticals = Array(cellsVertical)
   .fill(null)
-  .map(() => Array(cells - 1).fill(false));
+  .map(() => Array(cellsHorizontal - 1).fill(false));
 
-const horizontals = Array(cells - 1)
+const horizontals = Array(cellsVertical - 1)
 .fill(null)
-.map(() => Array(cells).fill(false));
+.map(() => Array(cellsHorizontal).fill(false));
 
-const startRow = Math.floor(Math.random() * cells);
-const startColumn = Math.floor(Math.random() * cells);
+const startRow = Math.floor(Math.random() * cellsVertical);
+const startColumn = Math.floor(Math.random() * cellsHorizontal);
 
 const moveThroughCell = (row, column) => {
   // If i have visited the cell at [row, column], then return
@@ -84,7 +86,7 @@ const moveThroughCell = (row, column) => {
     const [nextRow, nextColumn, direction] = neighbour;
 
   // See if the neighbour is out of bounds
-    if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+    if (nextRow < 0 || nextRow >= cellsVertical || nextColumn < 0 || nextColumn >= cellsHorizontal) {
       continue;
       // 'continue' doesnt break the for loop its just says to skip the rest of the code and loop back around
     }
@@ -117,10 +119,10 @@ horizontals.forEach((row, rowIndex) => {
       return;
     }
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength / 2,
-      rowIndex * unitLength + unitLength,
-      unitLength,
-      10,
+      columnIndex * unitLengthX + unitLengthX / 2,
+      rowIndex * unitLengthY + unitLengthY,
+      unitLengthX,
+      5,
       {
         label: 'wall',
         isStatic: true
@@ -137,10 +139,10 @@ verticals.forEach((row, rowIndex) => {
     }
 
     const wall = Bodies.rectangle(
-      columnIndex * unitLength + unitLength,
-      rowIndex * unitLength + unitLength / 2,
-      10,
-      unitLength,
+      columnIndex * unitLengthX + unitLengthX,
+      rowIndex * unitLengthY + unitLengthY / 2,
+      5,
+      unitLengthY,
       {
         label: 'wall',
         isStatic: true
@@ -153,10 +155,10 @@ verticals.forEach((row, rowIndex) => {
 // Goal
 
 const goal = Bodies.rectangle(
-  width - unitLength / 2,
-  height - unitLength / 2,
-  unitLength * .7,
-  unitLength * .7,
+  width - unitLengthX / 2,
+  height - unitLengthY / 2,
+  unitLengthX * 0.7,
+  unitLengthY * 0.7,
   {
     label: 'goal',
     isStatic: true
@@ -166,10 +168,11 @@ World.add(world, goal);
 
 // Ball
 
+const ballRadius = Math.min(unitLengthX, unitLengthY) / 4;
 const ball = Bodies.circle(
-  unitLength / 2,
-  unitLength / 2,
-  unitLength / 4,
+  unitLengthX / 2,
+  unitLengthY / 2,
+  ballRadius,
   {
     label: 'ball'
   }
